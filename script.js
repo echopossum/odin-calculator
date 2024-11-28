67e1c6fd9190efe22fce07d6c573ebba77117624
Echo <echopossum@gmail.com>
2024-11-28 13:11:09 -0500

    Add event listeners

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
 //
 //create event handlers for all the buttons
 //
+const container = document.querySelector('.container')
+container.addEventListener('click',(e) =>{
+    if (e.target.classList.contains('button')){
+        switch(true){
+            case e.target.classList.contains('numbers'):
+                console.log('number presssed')
+                break
+            case e.target.classList.contains('operatorButton'):
+                console.log('operator pressed')
+                break
+        }
+    }
+})
+
+function processNumer(id){
+
+}
+
+function processOperator(id){
+
+}
+
+function processFunctionKey(id){
+
+}
+
