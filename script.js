2fef87eb5ece87ce0ade7be57731a76cbed824d6
Echo <echopossum@gmail.com>
2024-11-28 13:36:03 -0500

    Pass number from eventlistener

--- script.js
+++ script.js
@@ -2,22 +2,35 @@
 //
 //create event handlers for all the buttons
 //
+let slots = [
+    {inUse: false, value: 0},
+    {inUse: false, value: 0}
+]
+let resultStr = ''
+
+const screen = document.querySelector('.screen')
+screen.textContent = resultStr
+
 const container = document.querySelector('.container')
 container.addEventListener('click',(e) =>{
     if (e.target.classList.contains('button')){
         switch(true){
             case e.target.classList.contains('numbers'):
                 console.log('number presssed')
+                processNumber(e.target.id)
                 break
             case e.target.classList.contains('operatorButton'):
                 console.log('operator pressed')
+                break
+            case e.target.classList.contains('funcButton'):
+                console.log('function pressed')
                 break
         }
     }
 })
 
-function processNumer(id){
-
+function processNumber(val){
+    console.log(val)
 }
 
 function processOperator(id){
